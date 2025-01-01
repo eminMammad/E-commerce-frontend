d647fb43adcdddbf7649e9c39647cb05b4149984
Emin Mammadzada <eminmemmedzade2001@gmail.com>
2025-01-01 18:32:02 +0300

    add: add latest 10 product item

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,23 +1,42 @@
-import react, {useContext} from 'react';
-import hero from "./Hero"
+import react, { useContext, useState, useEffect } from "react";
+import hero from "./Hero";
 import { ShopContext } from "../context/ShopContext";
 import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
 
-    const { products } = useContext(ShopContext);
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+  // console.log(products)
+  // console.log(currency);
 
-    // console.log(products)
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTION"} />
+        <p className="w-3/4 m-auto sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
 
-    return (
-        <div className='my-10'>
-            <div className='text-center py-8 text-3xl'>
-                <Title text1={"LATEST"} text2={"COLLECTION"}/>
-                {/* <p className="w-3/4 m-auto" */}
-            </div>
-        </div>
-    );
-
-}
+      {/* rendering products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default LatestCollection;
