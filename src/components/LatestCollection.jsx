ffbdfda86bead6e52361d434a9eb781845e4b9b2
Emin Mammadzada <eminmemmedzade2001@gmail.com>
2025-01-01 16:09:48 +0300

    fix: insert Title into Latest Collection

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,17 +1,20 @@
 import react, {useContext} from 'react';
 import hero from "./Hero"
 import { ShopContext } from "../context/ShopContext";
-
+import Title from "./Title";
 
 const LatestCollection = () => {
 
     const { products } = useContext(ShopContext);
 
-    console.log(products)
+    // console.log(products)
 
     return (
-        <div>
-
+        <div className='my-10'>
+            <div className='text-center py-8 text-3xl'>
+                <Title text1={"LATEST"} text2={"COLLECTION"}/>
+                {/* <p className="w-3/4 m-auto" */}
+            </div>
         </div>
     );
 
