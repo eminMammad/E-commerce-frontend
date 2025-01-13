7d1f26d0842c46f95095c4c71098020d2e7687b9
Emin Mammadzada <eminmemmedzade2001@gmail.com>
2025-01-14 02:51:18 +0300

    add: add cartItem function

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Cart = () => {
-    return (
-        <div>
-            <h1>Cart</h1>
-            <p>Welcome to the Cart page!</p>
-        </div>
-    );
-}
+  const { products, currency, cartItems } = useContext(ShopContext);
+
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempCartData = [];
+
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempCartData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    console.log(tempCartData);
+    setCartData(tempCartData);
+  }, [cartItems]);
+
+  return (
+    <div>
+      <h1>Cart</h1>
+      <p>Welcome to the Cart page!</p>
+    </div>
+  );
+};
 
 export default Cart;
