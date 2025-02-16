68fb763b1eef5334950ac939f2ebe76f2446d72a
Emin Mammadzada <eminmemmedzade2001@gmail.com>
2025-02-16 15:57:11 +0300

    Revert "fix:: fix home page products"
    This reverts commit 220912365a0fe4e1f879a7ba585471939d82f9b9.

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -78,31 +78,38 @@ const ShopContextProvider = (props) => {
     return totalAmount;
   }
 
-   const updateQuantity = async (itemId, size, quantity) => {
+  const getProductsData = async () => {
+    try {
+      const response = await axios.get(backendURL + "/api/product/list")
+      // console.log(response.data);
+
+      if (response.data.success) {
+        setProducts(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() =>{
+    getProductsData();
+  }, [])
+
+  useEffect( () => {
+    if (!token && localStorage.getItem("token")){
+      setToken(localStorage.getItem("token"));
+    }
+  })
+
+  const updateQuantity = async (itemId, size, quantity) => {
     let tempCartData = structuredClone(cartItems);
     tempCartData[itemId][size] = quantity;
 
     setCartItems(tempCartData);
   };
-
-const getProductsData = async () => {
-  try {
-    const response  = await axios.get(backendURL + "/api/product/list")
-    if(response.data.success){
-      setProducts(response.data.products);
-    } else {
-      toast.error(response.data.message);
-    }
-  } catch (error) {
-    console.log(error);
-    toast.error(error.message);
-  }
-}
-
-useEffect(() => {
-  getProductsData();
-}, [])
-
 
   const value = {
     products,
@@ -116,10 +123,13 @@ useEffect(() => {
     addToCart,
     getCartCount,
     updateQuantity,
+    setCartItems,
     getCartAmount, 
     navigate,
     backendURL,
-    };
+    setToken,
+    token
+  };
 
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
