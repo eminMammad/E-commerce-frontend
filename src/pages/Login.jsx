94937c531e3d471bc059585446d1e54e3b6ab45d
Emin Mammadzada <eminmemmedzade2001@gmail.com>
2025-02-16 16:06:45 +0300

    Revert "fix: fix user service api"
    This reverts commit 45f55a9d99d53ebc77db5f7c9b3d1975291f2154.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,10 +5,47 @@ import { toast } from "react-toastify";
 
 const Login = () => {
 
-    const[currentState, setCurrentState] = useState("Sign Up");
-        const onSubmitHandler = async (e) => {
+    const[currentState, setCurrentState] = useState("Login");
+    const {token, setToken, navigate, backendURL} = useContext(ShopContext);
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    const onSubmitHandler = async (e) => {
         e.preventDefault();
-    } 
+        try {
+            if (currentState === "Sign Up"){ //Sign Up API or register API
+                const response = await axios.post('/api/users/register', {username, email, password})
+                // console.log(response.data);
+                if (response.data.token) {
+                    setToken(response.data.token);
+                    localStorage.setItem("token", response.data.token);
+                } else {
+                    toast.error(response.data.message);
+                }
+            } else{ //Login API
+                const response = await axios.post('/api/users/login', {username, password});
+                // console.log(response.data);
+                if (response.data.token) {
+                    setToken(response.data.token);
+                    localStorage.setItem("token", response.data.token);
+                } else {
+                    toast.error(response.data.message);
+                }
+
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        if (token) {
+            navigate("/");
+        }
+    }, [token])
 
     return (
         <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
@@ -16,9 +53,9 @@ const Login = () => {
                 <p className="prata-regular text-3xl">{currentState}</p>
                 <hr className="border-none h-[1.5px] w-8 bg-gray-800"/>
             </div>
-            { currentState === "Login" ? "" : <input className="w-full px-3 py-2 border border-gray-800" placeholder="Name" type="text" required/>}
--            <input className="w-full px-3 py-2 border border-gray-800" placeholder="Email" type="email" required/>
--            <input className="w-full px-3 py-2 border border-gray-800" placeholder="Password" type="password" required/>
+            { currentState === "Login" ? "" : <input onChange={(e) => setEmail(e.target.value)} value={email} className="w-full px-3 py-2 border border-gray-800" placeholder="Email" type="email" required/>}
+            <input onChange={(e) => setUsername(e.target.value)} value={username} className="w-full px-3 py-2 border border-gray-800" placeholder="Name" type="text" required/>
+            <input onChange={(e) => setPassword(e.target.value)} value={password} className="w-full px-3 py-2 border border-gray-800" placeholder="Password" type="password" required/>
             <div className="w-full flex justify-between text-sm mt-[8px]">
                 <p className="cursor-pointer">
                     Forgot your password?
